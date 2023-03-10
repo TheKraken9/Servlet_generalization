97acabb7cf84c6f3c5373e15d46de292aa95e224
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-03-10 06:05:30 +0300

    insertion of servlet

---
+++ src/main/java/etu002663/framework/servlet/ServletGeneralization.java
@@ -0,0 +1,20 @@
+package etu002663.framework.servlet;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+
+@WebServlet(name = "ServletGeneralization", value = "/ServletGeneralization")
+public class ServletGeneralization extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
