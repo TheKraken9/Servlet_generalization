e0bb65ab22d2dfed49adf449bb72d8f3809553cf
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-03-12 18:55:19 +0300

    optimization, modification on sprint1

--- src/main/java/etu002663/framework/servlet/ServletGeneralization.java
+++ src/main/java/etu002663/framework/servlet/ServletGeneralization.java
@@ -8,13 +8,23 @@ import java.io.IOException;
 
 @WebServlet(name = "ServletGeneralization", value = "/ServletGeneralization")
 public class ServletGeneralization extends HttpServlet {
+
+    HashMap<String,Mapping> mappingUrl;
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = request.getRequestURI();
+        url = url.substring(request.getContextPath().length());
+        response.getWriter().write(url);
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 }
