6e4720d96cef9d6d49839d35bcbb2910d7e1fdcf
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-03-12 18:46:30 +0300

    insertion of modification

--- src/main/java/etu002663/framework/servlet/ServletGeneralization.java
+++ src/main/java/etu002663/framework/servlet/ServletGeneralization.java
@@ -3,18 +3,44 @@ package etu002663.framework.servlet;
 import jakarta.servlet.*;
 import jakarta.servlet.http.*;
 import jakarta.servlet.annotation.*;
+import mapping.Mapping;
 
 import java.io.IOException;
+import java.util.HashMap;
 
+/**
+ * The type Servlet generalization.
+ */
 @WebServlet(name = "ServletGeneralization", value = "/ServletGeneralization")
 public class ServletGeneralization extends HttpServlet {
+
+    /**
+     * The Mapping url.
+     */
+    HashMap<String, Mapping> mappingUrl;
+
+    /**
+     * Process request.
+     *
+     * @param request  the request
+     * @param response the response
+     * @throws ServletException the servlet exception
+     * @throws IOException      the io exception
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        String url = request.getRequestURI();
+        url = url.substring(request.getContextPath().length());
+        response.getWriter().write(url);
+    }
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        processRequest(request, response);
     }
 }
---
+++ src/main/java/mapping/Mapping.java
@@ -0,0 +1,51 @@
+package mapping;
+
+/**
+ * The type Mapping.
+ */
+public class Mapping {
+    /**
+     * The Class name.
+     */
+    String className;
+    /**
+     * The Method.
+     */
+    String method;
+
+    /**
+     * Gets class name.
+     *
+     * @return the class name
+     */
+    public String getClassName() {
+        return className;
+    }
+
+    /**
+     * Sets class name.
+     *
+     * @param className the class name
+     */
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    /**
+     * Gets method.
+     *
+     * @return the method
+     */
+    public String getMethod() {
+        return method;
+    }
+
+    /**
+     * Sets method.
+     *
+     * @param method the method
+     */
+    public void setMethod(String method) {
+        this.method = method;
+    }
+}
