2875031244d5bae77f4df8566f141f43d09f2faa
Andrianambinina Fenitra Tokiniaina <andrianambininafenitra@gmail.com>
2023-04-05 16:40:05 +0300

    commit for sprint 4

---
+++ NewFrame/TestFramework/Test/Emp.java
@@ -0,0 +1,11 @@
+package Test;
+
+import etu2663.framework.Url;
+
+public class Emp {
+    
+    @Url(url="find-All")
+    public void FindAll() {
+        System.out.println(" find_all ");
+    }
+}
