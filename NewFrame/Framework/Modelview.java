7240b502b38df3327a28e7bafc22ae517a870fe1
Andrianambinina Fenitra Tokiniaina <andrianambininafenitra@gmail.com>
2023-06-07 11:37:59 +0300

    commit for sprint 13

---
+++ NewFrame/Framework/Modelview.java
@@ -0,0 +1,43 @@
+package etu2663.framework;
+import java.util.HashMap;
+
+
+public class Modelview {
+    String view;
+    HashMap<String, Object> data = new HashMap<String, Object>();
+    HashMap<String, Object> session = new HashMap<String, Object>();
+    boolean gson = false;
+
+
+    public void setGson(boolean isGson) {
+        this.gson = isGson;
+    }
+    public boolean isGson() {
+        return gson;
+    }
+    public void setSession(HashMap<String, Object> session) {
+        this.session = session;
+    }
+    public HashMap<String, Object> getSession() {
+        return session;
+    }
+    public void setData(HashMap<String, Object> data) {
+        this.data = data;
+    }
+    public  HashMap<String, Object> getData(){
+        return this.data;
+    }
+    public String getView() {
+        return view;
+    }
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    public void addItem(String key, Object object) {
+        this.getData().put(key, object);
+    }
+    public void addSession(String key, Object object) {
+        this.getSession().put(key, object);
+    }
+}
