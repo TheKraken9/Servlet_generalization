e33281034f176de8013339149ce0acb84a3c98f2
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-07-05 02:24:48 +0300

    optimize code

--- src/main/java/etu002663/exception/JavaFileException.java
+++
@@ -1,7 +0,0 @@
-package etu2011.framework.exception;
-
-public class JavaFileException extends Exception {
-    public JavaFileException() {
-        super("ERROR: This is not a java file");
-    }
-}
