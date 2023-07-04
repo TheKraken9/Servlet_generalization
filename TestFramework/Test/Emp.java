973334ad7e00671aefc92571f67eaef5dbbbaf91
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-07-05 01:49:35 +0300

    update some file

---
+++ TestFramework/Test/Emp.java
@@ -0,0 +1,11 @@
+package Test;
+
+import etu1987.framework.Url;
+
+public class Emp {
+    
+    @Url(url="find-All")
+    public void FindAll() {
+        System.out.println(" find_all ");
+    }
+}
