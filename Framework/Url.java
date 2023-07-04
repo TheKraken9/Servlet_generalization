0409dda10133d5948a99cf1fc7acb482c23867f7
Andrianambinina Fenitra <andrianambininafenitra@gmail.com>
2023-07-05 02:10:08 +0300

    insertion of some modification

--- Framework/Url.java
+++
@@ -1,9 +0,0 @@
-package etu2663.framework;
-
-import java.lang.annotation.*;
-@Target(ElementType.METHOD)
-@Retention(RetentionPolicy.RUNTIME)
-
-public @interface Url {
-    String url() default " ";
-}
---
+++ test-framework/model/Emp.java
@@ -0,0 +1,10 @@
+package model;
+
+import etu2663.framework.annotation.Url;
+
+public class Emp {
+
+    @Url("test")
+    public void helloWorld() {
+    }
+}
